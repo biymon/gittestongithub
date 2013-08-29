fac7d2abb2fbbac8201e9851b1cbf6b8319e29d6
Mu <muliyuan@applematoMacBook-Pro.local>
2013-08-29 15:31:09 +0800

    Initial Commit

---
+++ gittest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  gittest
+//
+//  Created by Mu,Liyuan on 13-8-29.
+//  Copyright (c) 2013年 Mu,Liyuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ gittest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  gittest
+//
+//  Created by Mu,Liyuan on 13-8-29.
+//  Copyright (c) 2013年 Mu,Liyuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
