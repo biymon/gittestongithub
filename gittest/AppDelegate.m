2a32bfe08ceeed9bcb2d4d4f519212110ed4924a
lymu@nltechdev.com <lymu@nltechdev.com>
2013-09-04 16:15:21 +0800

    appdelegate modify

--- gittest/AppDelegate.m
+++ gittest/AppDelegate.m
@@ -15,6 +15,10 @@
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
+    
+    
+    
+    
     [self.window makeKeyAndVisible];
     return YES;
 }
