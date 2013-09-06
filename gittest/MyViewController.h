612090780bdd2cfa861ccedcb9ae976c76529c44
lymu@nltechdev.com <lymu@nltechdev.com>
2013-09-06 16:44:11 +0800

    add myviewcontroller files

---
+++ gittest/MyViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyViewController.h
+//  gittest
+//
+//  Created by Mu,Liyuan on 13-9-6.
+//  Copyright (c) 2013年 Mu,Liyuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyViewController : UIViewController
+
+@end
---
+++ gittest/MyViewController.m
@@ -0,0 +1,38 @@
+//
+//  MyViewController.m
+//  gittest
+//
+//  Created by Mu,Liyuan on 13-9-6.
+//  Copyright (c) 2013年 Mu,Liyuan. All rights reserved.
+//
+
+#import "MyViewController.h"
+
+@interface MyViewController ()
+
+@end
+
+@implementation MyViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
